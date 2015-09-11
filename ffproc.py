9a5bbb22921e332d23e94256aa15022fb43b1d4a
Alex Roth <me@magmastone.net>
2015-09-10 20:47:06 -0400

    2to3 changes

--- ffproc.py
+++ ffproc.py
@@ -159,9 +159,9 @@ for stream in streams_final:
 			ffmpeg.append("-c:v")
 			ffmpeg.append("libx264")
 			ffmpeg.append("-crf")
-                        ffmpeg.append("20")
-                        ffmpeg.append("-level:v")
-                        ffmpeg.append("4.0")
+			ffmpeg.append("20")
+			ffmpeg.append("-level:v")
+			ffmpeg.append("4.0")
 			ffmpeg.append("-preset")
 			ffmpeg.append(preset)
 		else: 

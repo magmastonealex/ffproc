def18ccf94b25a129e1f3ff42e1d3a4262002086
Alex Roth <alex@magmastone.net>
2016-09-05 23:42:52 -0400

    Worker now detects libfdk_aac and responds to force_libfdk option

--- worker.py
+++ worker.py
@@ -2,12 +2,24 @@ from task import Task
 import subprocess
 import os
 import shutil
+import sys
 
 def ffmpeg(arg):
 	
 	torun = Task(createfrom=arg)
 
-#	codeccheck = subprocess.Popen(["ffmpeg", "-codecs"], stdout=subprocess.PIPE)
+	codeccheck = subprocess.Popen(["ffmpeg", "-codecs"], stdout=subprocess.PIPE)
+	allcodecs = codeccheck.communicate()[0]
+	if allcodecs.find("libfdk_aac") == -1:
+		print "Install libfdk_aac for better audio quality!"
+		if torun.forcefdk == True:
+			print "Server has disabled workers without libfdk_aac!"
+			sys.exit()
+		else:
+			libfdk_pos = torun.arguments.find("libfdk_aac")
+			torun.arguments[libfdk_pos] = "aac"
+			torun.arguments.append("-strict")
+			torun.arguments.append("-2")
 
 	out=subprocess.call(["ffmpeg","-i", torun.infile]+torun.arguments+["tmp.mp4"])
 	if not out == 0:

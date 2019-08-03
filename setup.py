1b370b5c7801e43e0a249a133cce5b5bf7b6b0e6
root <root@media-server.moodysalon.net>
2019-08-03 02:49:08 -0700

    Python: Update project distribution metadata

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
+#!/usr/bin/env python
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="ffproc",
+    version="1.0",
+    author="Alex Roth",
+    author_email="alex@magmastone.net",
+    description=(
+        "A batch transcoding system. Supports multiple remote workers."),
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/magmastonealex/ffproc",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "Natural Language :: English",
+        "Topic :: Multimedia :: Video :: Conversion",
     ],
 )
